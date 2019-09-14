5e28c04c3487cb56b25b3650466d1b29c3544ae0
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2019-09-14 13:38:23 +0300

    Project,User models created.

---
+++ Controllers/Models/User.cs
@@ -0,0 +1,18 @@
+namespace Backend.Controllers.Models
+{
+    public class User
+    {
+        public int id { get; set; } 
+        public bool isdeleted { get; set; }
+        public bool isnotusing { get; set; }
+
+        public string email { get; set; }   
+        public string password { get; set; }
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public string username { get; set; }
+        public string profileurl { get; set; }
+        public int[] rightids { get; set; }
+
+    }
+}
